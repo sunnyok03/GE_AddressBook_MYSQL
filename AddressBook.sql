561f0f632d576e0bfcde53de300593f2f2c9a358
sunnyok03 <sunnyok03@gmail.com>
2023-12-14 07:33:11 +0530

    UC2 created address book table

--- AddressBook.sql
+++ AddressBook.sql
@@ -1,2 +1,15 @@
-CREATE DATABASE AddresBook;
+# USE CASE 1
+CREATE DATABASE AddresBookService;
 
+# USE CASE 2
+CREATE TABLE AddressBook(
+	FirstName varchar(100),
+    LastName varchar(100),
+    Address varchar(200),
+    City varchar(50),
+    State varchar(50),
+    ZipCode varchar(20),
+    PhoneNumber varchar(20),
+    Email varchar(50),
+    PRIMARY KEY (FirstName,LastName)
+);
